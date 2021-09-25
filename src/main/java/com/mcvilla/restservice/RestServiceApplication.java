a0be8aabba4f08b4a4ea9a6e4c6f7d2f5cbc3256
ahmdsaleem <ahmdsaleem@hotmail.com>
2021-09-25 21:02:09 +0500

    test commit

--- src/main/java/com/mcvilla/restservice/RestServiceApplication.java
+++ src/main/java/com/mcvilla/restservice/RestServiceApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestServiceApplication {
 
     public static void main(String[] args) {
+
+        System.out.println("hello");
         SpringApplication.run(RestServiceApplication.class, args);
     }
 
